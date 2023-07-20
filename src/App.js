b0f8f50244c1a9246204464d8128c9b78f08cec2
jswan <jswannieman@gmail.com>
2023-07-20 18:54:47 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [text, setText] = useState("");
+  const [textChange, setTextChange] = useState(false);
+
+  useEffect(() => {
+    setTextChange(true);
+  }, [text]);
+
+  return (
+    <div className="App">
+      <div className="buttons">
+        <button onClick={() => setText("Hello")}>Button1</button>
+        <button onClick={() => setText("Everyone")}>Button2</button>
+        <button onClick={() => setText("Who Loves")}>Button3</button>
+        <button onClick={() => setText("Programming")}>Button4</button>
+      </div>
+      {textChange && <h4>The Text has changed to {text}</h4>}
+    </div>
+  );
 }
 
 export default App;
